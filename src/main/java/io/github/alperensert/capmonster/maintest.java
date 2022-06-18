2d1bae54deb39f4c8ff7e1217ab565928e0edad8
Anjesh Shrestha <anjeshshrestha@gmail.com>
2022-06-18 03:12:16 -0600

    update pom

--- src/main/java/io/github/alperensert/capmonster/maintest.java
+++ src/main/java/io/github/alperensert/capmonster/maintest.java
@@ -7,19 +7,6 @@ import java.net.URL;
 
 public class maintest {
     public static void main(String[] args){
-        try {
-            URL url = new URL("https://darkorbit.com/");
-//            String frontPage = IOUtils.read(Http.create(url.toString()).getInputStream());
-            CaptchaSolver temp = new CaptchaSolver();
-            CaptchaSolver.url = url.toString();
-//            TwoCaptcha solver = new TwoCaptcha();
-//            ReCaptcha captcha = new ReCaptcha();
-//            solver.setApiKey("472422416327eb9904a58c76e462ed2c");
-//            System.out.println(solver.getResult("70741716661"));
-            String answer = temp.getCaptchaResponse("6LfkgUIUAAAAAETf-SZEx_exK2SEPirE8i2RZQ_U");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 //        CapMonsterClient test = new CapMonsterClient("6d52cf1e80b57c2b91221874f3d6c773");
 //        System.out.println(test.getBalance());
 //
